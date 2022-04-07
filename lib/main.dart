4bf58da32bd012e49e477333db6ee679568e35dc
Rohit Rajput <rohit.megamax1998@gmail.com>
2022-04-07 12:28:15 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/registration_screen.dart';
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme:TextTheme(
+          bodyText1:TextStyle(
+            color: Colors.black54
+          )
+        )
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id:(context)=>WelcomeScreen(),
+        LoginScreen.id:(context)=>LoginScreen(),
+        ChatScreen.id:(context)=>ChatScreen(),
+        RegistrationScreen.id:(context)=>RegistrationScreen(),
+      },
+    );
+  }
+}
+
