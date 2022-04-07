78d0e2630decad853f25cb0a4d2685838a2a44f7
Rohit Rajput <rohit.megamax1998@gmail.com>
2022-04-07 12:30:18 +0530

    New Update

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -65,6 +65,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
                   borderRadius: BorderRadius.all(Radius.circular(32.0)),
                 ),
+
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
                   borderRadius: BorderRadius.all(Radius.circular(32.0)),
